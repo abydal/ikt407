89cfc2a618e3590b3428d32cc0b0610fe0ff1066
asbjorn.bydal <asbjorn.bydal@77f1973c-bb3b-0410-be00-45c069678753>
2007-10-10 12:07:09 +0000

    - Updated Astar.cpp and AStar.h

--- trunk/Source/Astar.cpp
+++ trunk/Source/Astar.cpp
@@ -1,30 +1,36 @@
-#include "AStar.h"
+#include "../Header/AStar.h"
 
 using namespace std;
 
 void AStar::FindPath(point posA, point posB)
 {
 	square start;
-	start.pos.x = posA.x;
-	start.pos.y = posA.y;
+	start.position.x = posA.x;
+	start.position.y = posA.y;
 	start.gValue = 0;
 	start.hValue = 0;
 
-
-
+	square destination;
+	destination.position.x = posA.x;
+	destination.position.y = posA.y;
+	destination.gValue = 0;
+	destination.hValue = 0;
 }
 
-void AStar::updateLists(square* pos)
-{
-	
-}
 
 square* AStar::FindPoint(point* pos)
 {
-	for(int i = 0; i < AStar::openList.size; i++)
+	for(int i = 0; i < (int)openList.size(); i++)
 	{
-		if(openList[i]->pos.x == pos->x && openList[i]->pos.y == pos->y)
-			return openList[i];
+	//	if(openList[i]->position.x == pos->x && openList[i]->position.y == pos->y)
+	//		return openList[i];
+		cout << pos->x;
 	}
 	return NULL;
 }
+
+int AStar::CalculateManhattan(point posA, point posB)
+{
+	return fabs((double)posA.x - (double)posB.x)*10 + fabs((double)posA.y - (double)posB.y)*10;
+}
+
