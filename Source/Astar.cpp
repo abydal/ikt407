d9be8d87257a7eef94a88af43a67fbf450dcc87b
asbjorn.bydal <asbjorn.bydal@77f1973c-bb3b-0410-be00-45c069678753>
2007-10-10 15:43:02 +0000

    Update - more work on Astar.cpp and AStar.h

--- Source/Astar.cpp
+++ Source/Astar.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 void AStar::FindPath(point posA, point posB)
 {
-	square start;
+	diagonalCost = 14;
+	straightCost = 10;
+
 	start.position.x = posA.x;
 	start.position.y = posA.y;
 	start.gValue = 0;
 	start.hValue = 0;
 
-	square destination;
 	destination.position.x = posA.x;
 	destination.position.y = posA.y;
 	destination.gValue = 0;
@@ -18,13 +19,22 @@ void AStar::FindPath(point posA, point posB)
 }
 
 
-square* AStar::FindPoint(point* pos)
+square* AStar::InOpenList(point* pos)
 {
 	for(int i = 0; i < (int)openList.size(); i++)
 	{
-	//	if(openList[i]->position.x == pos->x && openList[i]->position.y == pos->y)
-	//		return openList[i];
-		cout << pos->x;
+		if(openList[i]->position.x == pos->x && openList[i]->position.y == pos->y)
+			return openList[i];
+	}
+	return NULL;
+}
+
+square* AStar::InClosedList(point* pos)
+{
+	for(int i = 0; i < (int)closedList.size(); i++)
+	{
+		if(closedList[i]->position.x == pos->x && closedList[i]->position.y == pos->y)
+			return closedList[i];
 	}
 	return NULL;
 }
@@ -32,5 +42,67 @@ square* AStar::FindPoint(point* pos)
 int AStar::CalculateManhattan(point posA, point posB)
 {
 	return fabs((double)posA.x - (double)posB.x)*10 + fabs((double)posA.y - (double)posB.y)*10;
+}
+
+void AStar::CalculateAdjecentSquares(square* parent)
+{
+	for(int x = -1; x < 2;x++)
+	{
+		for (int y = -1; y < 2; y++)
+		{
+			if(x == 0 && y == 0)
+				continue;
+
+			point tempPoint;
+			tempPoint.x = x + parent->position.x;
+			tempPoint.y = y + parent->position.y;
+			
+			if(IsWalkable() && !InClosedList(tempPoint))
+			{
+				square *tempSquare = InOpenList(tempPoint);
+				square newSquare;
+
+				if(temp == NULL)
+				{
+					//Insert new square into open list
+					newSquare.parent = parent;
+					newSquare.position.x = tempPoint.x;
+					newSquare.position.y = tempPoint.y;
+					newSquare.hValue = CalculateManhattan(newSquare.position,destination.position);
+					if((fabs(x) + fabs(y)) == 2)
+					{
+						newSquare.gValue = parent->gValue + diagonalCost;
+					}
+					else
+					{
+						newSquare.gValue = parent->gValue + straightCost;
+					}
+					newSquare.fValue = newSquare.hValue + newSquare.gValue;
+
+					openList.insert(newSquare);
+
+				}
+				else
+				{
+					if(tempSquare->gValue > newSquare.gValue)
+					{
+						//Uncertain about this
+						tempSquare->parent = parent;
+						if((fabs(x) + fabs(y)) == 2)
+						{
+							newSquare.gValue = parent->gValue + diagonalCost;
+						}
+						else
+						{
+							newSquare.gValue = parent->gValue + straightCost;
+						}
+					}
+
+				}
+
+			}
+
+		}
+	}
 }
 
